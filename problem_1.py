1af4d6058739350a3a2fd6611d7681b3e0e707b2
Shakib Ibna Shameem Polock <50078050+polock11@users.noreply.github.com>
2023-11-16 17:16:09 +0200

    contains_duplicate_array_problem

--- problem_1.py
+++ problem_1.py
@@ -1,1 +1,17 @@
-print('Hello')
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+                
+        return False
+    
+nums = [1,2,3]
+solution = Solution()
+val = solution.containsDuplicate(nums)
+print(val)
